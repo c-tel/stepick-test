efb84d49c2167567c43a9f89f740861104071ddd
c-tel <rorex555@yandex.ru>
2017-07-24 13:44:34 +0300

    test

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/hello.py
+gunicorn -b 0.0.0.0:8080 hello:app
 sudo /etc/init.d/gunicorn restart
 ﻿sudo /etc/init.d/mysql start﻿
