af279fdc8eb5d76d659a03827f93160b004f1aed
c-tel <rorex555@yandex.ru>
2017-07-25 14:38:23 +0300

    test

--- init.sh
+++ init.sh
@@ -1,6 +1,3 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo unlink /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/hello.py
 gunicorn -b 0.0.0.0:8080 hello:app
-sudo /etc/init.d/gunicorn restart
-﻿sudo /etc/init.d/mysql start﻿
