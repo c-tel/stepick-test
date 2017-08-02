246856e838d1acae502c0d8c93ef526e67a03800
c-tel <rorex555@yandex.ru>
2017-08-02 15:23:19 +0300

    myc

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -8,7 +8,7 @@ class Question(models.Model):
     title = models.CharField(max_length=255)             
     text = models.TextField()                         
     added_at = models.DateTimeField(auto_now_add=True)
-	rating = models.IntegerField(default = 0);
+	rating = models.IntegerField(default=0);
 	author = models.ForeignKey(User)
 	likes = models.ManyToManyField(User)                 
 
