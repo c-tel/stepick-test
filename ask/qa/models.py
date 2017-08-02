d859c213cf0f0e0f34eab29bd6d9743fd5c418fc
c-tel <rorex555@yandex.ru>
2017-08-02 15:04:24 +0300

    myc

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,5 +1,32 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import django.contrib.auth.models.User
 
 # Create your models here.
+class Question(models.Model):                                      
+    title = models.CharField(max_length=255)             
+    text = models.TextField()                         
+    added_at = models.DateTimeField(auto_now_add=True)
+	rating = models.IntegerField(default = 0);
+	author = models.ForeignKey(User)
+	likes = models.ManyToManyField(User)                 
+
+class QuestionManager(models.Manager):
+	def sortByDate(question):
+        return question.added_at 
+	def new(self):
+		return self.all().sort(key = sortByDate)
+	def sortByPop(question):
+		return question.rating
+	def popular(self):
+		return self.all().sort(key = sortByPop)
+		
+	
+class Answer(models.Model):                                      
+    text = models.TextField()                         
+    added_at = models.DateTimeField(auto_now_add=True)
+	author = models.ForeignKey(User)
+	question = models.OneToOneField(Question)                 
+
+	
