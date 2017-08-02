437e3454cbef0124e2e8b98ba11e4d8cb7231edf
c-tel <rorex555@yandex.ru>
2017-08-02 16:53:24 +0300

    myc

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,10 +1,10 @@
+from django.contrib.auth.models import User
+
 from __future__ import unicode_literals
 
 from django.db import models
 
-from django.contrib.auth.models import User
 
-# Create your models here.
 class Question(models.Model):                                      
 	title = models.CharField(max_length=255)             
 	text = models.TextField()                         
