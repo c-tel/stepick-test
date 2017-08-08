c276d1f67e470acd33a437b7513d394c573b3e21
c-tel <rorex555@yandex.ru>
2017-08-08 16:34:55 +0300

    nnn

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 # Create your views here.
 from django.http import HttpResponse 
