2ca71d3f0d3d73520d6b25401b5f5bbc550b321a
c-tel <rorex555@yandex.ru>
2017-08-10 14:34:59 +0300

    forms

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -3,6 +3,7 @@ from django.core.paginator import Paginator
 # Create your views here.
 from django.http import HttpResponse 
 from models import Question, Answer
+from formss import QuestionForm, AnswerForm
 def main(request):    
     limit = 10
     try:
@@ -30,9 +31,28 @@ def popular(request):
 	})
 def question(request, id):    
 	question = get_object_or_404(Question, pk=id)
+	if request.method == 'POST':
+		form = AnswerForm(request.POST)
+		if form.is_valid():
+			answer = form.save()
 	answer_list = question.answer_set.order_by('-added_at')
+	form = AnswerForm()
 	return render(request, 'question.html',{
 		'title' : question.title,
 		'quest' : question,
-		'answer_list' : answer_list
+		'answer_list' : answer_list,
+		'form': form,
+		})
+def ask(request):
+	def post_add(request):
+	if request.method == "POST":
+		form = QuestionForm(request.POST)
+		if form.is_valid():
+			question = form.save()
+			url = '/question/' + str(question.id)
+			return HttpResponseRedirect(url)
+	else:
+		form = QuestionForm()
+	return render(request, 'ask.html', {
+		'form': form,
 		})
